f494d7fcdd6264c49d2684c25bfafdf5c6dfe6d8
Julian Darou-Santos <juliandarou@icloud.com>
2025-02-09 16:19:37 -0500

    Setup pio and include scripts

---
+++ include/config_manager.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <EEPROM.h>
+
+class ConfigManager {
+public:
+  struct Config {
+    char wifiSSID[32];
+    char wifiPass[32];
+    char githubPAT[40];
+  };
+
+  void loadConfig();
+  void saveConfig();
+  
+  const char* getWiFiSSID() const;
+  const char* getWiFiPass() const;
+  const char* getGitHubPAT() const;
+
+private:
+  Config config;
+};
