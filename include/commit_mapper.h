205cfe0449b6de3b6ec443a045921e2147046147
Julian Darou-Santos <juliandarou@icloud.com>
2025-02-12 19:24:59 -0500

    updated .h files to match project goals

--- include/commit_mapper.h
+++ include/commit_mapper.h
@@ -4,6 +4,9 @@
 
 class CommitMapper {
 public:
+  // Brightness tiers: 0 (off), 63 (25%), 127 (50%), 255 (100%)
   static const int BRIGHTNESS_LEVELS[4];
-  static void normalizeBrightness(uint8_t commits[30]);
+  
+  // Raw commit counts (for 32 days) to brightness values
+  static void normalizeBrightness(uint8_t commits[32]);
 };
