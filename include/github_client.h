fe2c2eba9377c5796c6ea12ade26452c6bd5d2db
Julian Darou-Santos <juliandarou@icloud.com>
2025-02-25 23:46:15 -0500

    Reworked gitclient for a future change

--- include/github_client.h
+++ include/github_client.h
@@ -1,15 +1,12 @@
 #pragma once
 #include <Arduino.h>
-#include <ESP8266HTTPClient.h>
 
 class GitHubClient {
 public:
-  // Initialize WiFi connection using provided credentials
   bool initialize(const char* ssid, const char* password);
-  
-  // Fetch commit data (for the last 32 days) using GitHub username and PAT
   bool fetchCommitData(uint8_t commits[32], const char* githubUsername, const char* githubPAT);
   
 private:
-  static const char* GITHUB_API_ROOT;
+  static const char* GITHUB_API_HOST;
+  static const int HTTPS_PORT;
 };
